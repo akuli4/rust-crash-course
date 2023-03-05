44b17b9e05a8b2bf9ace1948bab9a5c060207c40
akuli4 <fkdrsp@proton.me>
2023-03-05 18:11:53 +0100

    Static struct methods example

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 #![deny(clippy::all)]
 
 fn main() {
+    #[derive(Debug)]
     struct Point(f32, f32, f32);
+
     impl Point {
-        fn twice(&self) -> Point {
-            Point(self.0 * 2.0, self.1 * 2.0, self.2 * 2.0)
+        // Non method associated function
+        fn zero() -> Point {
+            Point(0.0, 0.0, 0.0)
         }
     }
 
-    let x = Point(2.0, 10.0, 3.0);
-    let y = x.twice();
+    impl Point {
+        // fn twice(&self) -> Point {
+        //     Point(self.0 * 2.0, self.1 * 2.0, self.2 * 2.0)
+        // }
+    }
 
-    println!(
-        "Point: ({}, {}, {}), twice call: {} {} {}",
-        x.0, x.1, x.2, y.0, y.1, y.2
-    );
+    let x = Point::zero();
+    let y = Point::zero();
+    let z = Point::zero();
+
+    println!("x: {:?}, y: {:?}, z: {:?}", x, y, z);
 }
