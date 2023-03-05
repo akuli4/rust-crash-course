99b61be0d7480289a24fae871e0d9833e5a23f93
akuli4 <fkdrsp@proton.me>
2023-03-05 18:04:47 +0100

    Struct example without modifying the point

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,18 @@
 #![deny(clippy::all)]
 
 fn main() {
-    struct User {
-        first_name: String,
-        family_name: String,
-        picture_url: String,
-    }
-    impl User {
-        fn full_name(&self) -> String {
-            format!("{} {}", self.first_name, self.family_name)
+    struct Point(f32, f32, f32);
+    impl Point {
+        fn twice(&self) -> Point {
+            Point(self.0 * 2.0, self.1 * 2.0, self.2 * 2.0)
         }
     }
-    let user1 = User {
-        first_name: "Darth".to_string(),
-        family_name: "Vader".to_string(),
-        picture_url: "https://akamai.darth-vader-pics.bruh/a29aDMas9dsl".to_string(),
-    };
+
+    let x = Point(2.0, 10.0, 3.0);
+    let y = x.twice();
 
     println!(
-        "
-            first_name: {},
-            family_name: {},
-            picture_url: {}
-            full_name: {}
-        ",
-        user1.first_name,
-        user1.family_name,
-        user1.picture_url,
-        user1.full_name()
+        "Point: ({}, {}, {}), twice call: {} {} {}",
+        x.0, x.1, x.2, y.0, y.1, y.2
     );
 }
