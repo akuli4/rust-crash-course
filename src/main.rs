dd21588cd04ea5b8b60e72913231927ed35128cc
akuli4 <fkdrsp@proton.me>
2023-03-05 20:07:48 +0100

    Advanced enum example with tuple as a struct parameter

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@
 
 #[derive(PartialEq)]
 enum Shapes {
-    Circle { radius: u32 },
+    Circle { radius: u32, center: (i32, i32) },
     Triangle { base: u32, height: u32 },
     Rectangle { left_side: u32, right_side: u32 },
     Point(i32, i32, i32),
 }
 
 fn main() {
-    let circle = Shapes::Circle { radius: 20 };
+    let circle = Shapes::Circle {
+        radius: 20,
+        center: (20, 20),
+    };
     let triangle = Shapes::Triangle {
         base: 10,
         height: 20,
@@ -18,10 +21,13 @@ fn main() {
         left_side: 6,
         right_side: 8,
     };
-    let point = Shapes::Point(2, 3, 4)
+    let point = Shapes::Point(2, 3, 4);
 
     match circle {
-        Shapes::Circle { radius: 20 } => {
+        Shapes::Circle {
+            radius: 20,
+            center: (20, 20),
+        } => {
             println!("Circle Match")
         }
         _ => println!("Not Covered"),
