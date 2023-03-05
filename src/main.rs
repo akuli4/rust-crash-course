fb1e55afbf884a91c9b96d5d109001b4b31e1962
akuli4 <fkdrsp@proton.me>
2023-03-05 11:56:56 +0100

    Function example 3

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 #![deny(clippy::all)]
 
-fn multiply(num1: i32, num2: i32) -> i32 {
-    num1 * num2
+fn generate_hello_statement(string: &str) -> String {
+    format!("Hi, {}!", string)
 }
-//  (-> ()) === (=> void)
 
 fn main() {
-    let num1 = 2;
-    let num2 = 3;
-    println!(
-        "The product of {0} and {1} is {2}",
-        num1,
-        num2,
-        multiply(num1, num2)
-    );
+    let bar = generate_hello_statement("Alan");
+
+    println!("{}", bar)
 }
