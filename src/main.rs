7f6b2e360afdd8371596d00d129666606b5b90bb
akuli4 <fkdrsp@proton.me>
2023-03-05 14:42:30 +0100

    Tuple struct example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let calculate_bmi = |weight: f32, height: f32| weight / height.powf(2.0);
+    struct Point(f32, f32, f32);
+    impl Point {
+        fn double(&mut self) {
+            self.0 *= 2.0;
+            self.1 *= 2.0;
+            self.2 *= 2.0;
+        }
+    }
 
-    println!("Your bmi is - {:.2}", calculate_bmi(70.0, 1.86));
+    let mut x = Point(2.0, 10.0, 3.0);
+    x.double();
+
+    println!("Point: ({}, {}, {})", x.0, x.1, x.2);
 }
