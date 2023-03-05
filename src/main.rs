953366a52b8e2f4d228a4ad0da421436d7a27833
akuli4 <fkdrsp@proton.me>
2023-03-05 11:46:38 +0100

    Function example 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 #![deny(clippy::all)]
 
-fn say_anything(string: &str) {
-    println!("{}", string)
+fn multiply(num1: i32, num2: i32) -> i32 {
+    num1 * num2
 }
 
 fn main() {
-    say_anything("Hello World")
+    let num1 = 2;
+    let num2 = 3;
+    println!(
+        "The product of {0} and {1} is {2}",
+        num1,
+        num2,
+        multiply(num1, num2)
+    );
 }
