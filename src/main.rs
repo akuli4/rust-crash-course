dc45ba57d39370b3b04218ffdc89306a03b5a103
akuli4 <fkdrsp@proton.me>
2023-03-07 12:00:44 +0100

    Use hash maps as values in a hash map

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 #![deny(clippy::all)]
 
 use std::collections::HashMap;
+
+struct User {
+    id: i128,
+    firs_name: String,
+    family_name: String,
+}
 fn main() {
     // Hash maps are not enabled by default, we need to import them.
-    let mut user: HashMap<&str, &str> = HashMap::new();
-    user.insert("first_name", "Anna");
-    user.entry("family_name").or_insert("Karenina");
+    let mut users: HashMap<i128, User> = HashMap::new();
+
+    users.insert(
+        0,
+        User {
+            id: 0,
+            firs_name: "Anna".to_string(),
+            family_name: "Karenina".to_string(),
+        },
+    );
+
+    let first_user = users.get(&0);
 }
