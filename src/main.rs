1b1d23f74fa5509c553686fc31f126cef4f4256a
akuli4 <fkdrsp@proton.me>
2023-03-07 11:45:02 +0100

    Insert if non existent, hash map example

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,6 @@ use std::collections::HashMap;
 fn main() {
     // Hash maps are not enabled by default, we need to import them.
     let mut user: HashMap<&str, &str> = HashMap::new();
-    user.insert("foo", "bar");
-    user.insert("boo", "far");
-
-    let entry = user.entry("foo"); // entry.key(), entry.into()
-
-    match entry {
-        std::collections::hash_map::Entry::Occupied(mut o) => {
-            o.get_mut();
-        }
-        _ => {}
-    }
+    user.insert("first_name", "Anna");
+    user.entry("family_name").or_insert("Karenina");
 }
