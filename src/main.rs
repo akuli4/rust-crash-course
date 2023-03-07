cb93744323ec6b0144af3b6b9fd84f6686a77613
akuli4 <fkdrsp@proton.me>
2023-03-07 12:16:44 +0100

    Previous error example

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ fn main() {
     /*
        Iterators are lazy, they need to be consumed before can be user.
        In rust a for loop automatically consumes and iterator.
+
+       Iterators cannot be double-consumed.
+
+       The pointer is at the end after first iteration and stays there.
     */
     let values = vec![2, 4, 2, 1];
-    for value in values.iter() {
-        println!("{}", value);
-    }
+    let iter = values.iter();
+    let sum1: i32 = iter.sum();
+    let sum2: i32 = iter.sum(); // use of a moved value
 }
