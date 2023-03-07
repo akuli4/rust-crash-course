a1ee1e7110d7f5d2b44007b40ee24c72856ca1a6
akuli4 <fkdrsp@proton.me>
2023-03-07 10:39:02 +0100

    Functions can return tuples

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let tuple = (2, 2.3, "Hi");
+    let tuple = get_values();
     // tuples are heteregeneous, meaning they are of diverse in character or content.
     // destructuring
     let (_, _, string) = tuple;
+
+    fn get_values() -> (i32, f32, String) {
+        (2, 2.3, String::from("Hello"))
+    }
 }
