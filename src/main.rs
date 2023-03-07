dee6d4b48b0e93aa309b443c3d19dd75d5f16042
akuli4 <fkdrsp@proton.me>
2023-03-07 10:35:51 +0100

    Tuple example 1

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 #![deny(clippy::all)]
 
-enum Pet {
-    Cat { name: String },
-    Dog { name: String },
-}
 fn main() {
-    let bobby = Pet::Cat {
-        name: "Bobby".to_string(),
-    };
-    // How to extract pets name?
-    // match is an expression
-    let name = match bobby {
-        Pet::Cat { name } => name,
-        Pet::Dog { name } => name,
-    };
-
-    println!("Name is {}", name);
+    let tuple = (2, 2.3, "Hi");
+    // tuples are heteregeneous, meaning they are of diverse in character or content.
+    // destructuring
+    let (num, float, string) = tuple;
 }
