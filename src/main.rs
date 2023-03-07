c017b559eae1f16202b045c5fc11b746794a48f7
akuli4 <fkdrsp@proton.me>
2023-03-07 11:09:04 +0100

    Check if a vector contains specific value

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let mut vec1 = vec![1, 2, 3];
-    let mut vec2 = vec![4, 5, 6];
+    let vec2 = vec![4, 5, 6];
 
-    // Move values from vec2 to vec1
-    vec1.append(&mut vec2);
-
-    // vec2 is empty now
+    if vec2.contains(&4) {
+        println!("contains 4")
+    };
 }
