46c70758d7bedaa24589faf113212706bc000787
akuli4 <fkdrsp@proton.me>
2023-03-07 11:33:58 +0100

    Iterate over object entries

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ fn main() {
     // Hash maps are not enabled by default, we need to import them.
     let mut user: HashMap<&str, &str> = HashMap::new();
     user.insert("foo", "bar");
+    user.insert("boo", "far");
 
-    match user.get("foo") {
-        Some(value) => println!("{}", value),
-        None => println!("Not Found"),
+    for (&k, &v) in &user {
+        println!("key: {}, value: {}", k, v);
     }
 }
