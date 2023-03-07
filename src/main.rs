709cb622569a4f15eeaa9426e6b5be855775e495
akuli4 <fkdrsp@proton.me>
2023-03-07 10:55:16 +0100

    Dynamic sized vector example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 #![deny(clippy::all)]
 
 fn main() {
-    // Fix sized vector of string slices.
-    let vector: [i32; 2] = [2, 3];
+    // Dynamic sized vector of string slices.
+    let vector = vec![2, 3];
+    //or
+    let mut vector = Vec::new();
+
+    vector.push(42);
+    vector.push(32);
+    vector.push(10);
 
     let mapped = vector.iter().map(|x| x * 2);
 }
