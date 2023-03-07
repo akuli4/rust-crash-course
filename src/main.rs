f9481078867e08927ff3bf4f62341ca8c88541e3
akuli4 <fkdrsp@proton.me>
2023-03-07 10:44:41 +0100

    Fixed size vector (array)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let tuple = get_values();
-    // tuples are heteregeneous, meaning they are of diverse in character or content.
-    // destructuring
-    let (_, _, string) = tuple;
-
-    fn get_values() -> (i32, f32, String) {
-        (2, 2.3, String::from("Hello"))
-    }
+    // Fix sized vector of string slices.
+    let vector: [&str; 2] = ["foo", "bar"];
 }
