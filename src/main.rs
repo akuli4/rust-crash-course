1b94e7a51db9a038818c02f9268b99032513b36a
akuli4 <fkdrsp@proton.me>
2023-03-07 11:16:21 +0100

    Check hashMap key existence

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use std::collections::HashMap;
 fn main() {
     // Hash maps are not enabled by default, we need to import them.
     let mut user: HashMap<&str, &str> = HashMap::new();
-
     user.insert("foo", "bar");
 
-    println!("{}", user["foo"]);
+    if user.contains_key("foo") {
+        println!("Something...")
+    } else {
+        println!("Empty")
+    };
 }
