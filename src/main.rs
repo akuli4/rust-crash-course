8f1347033bdb00411b2aeaf9a77e478488026268
akuli4 <fkdrsp@proton.me>
2023-03-07 10:46:08 +0100

    Grab a reference value from a vactor

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,5 @@ fn main() {
     // Fix sized vector of string slices.
     let vector: [&str; 2] = ["foo", "bar"];
 
-    for value in vector.iter() {
-        println!("{}", value)
-    }
+    let foo = &vector[0];
 }
