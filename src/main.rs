c6956c0fa7c06a6278fe80640cfabe2e71722a5b
akuli4 <fkdrsp@proton.me>
2023-03-07 11:13:36 +0100

    Hash map example 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![deny(clippy::all)]
 
+use std::collections::HashMap;
 fn main() {
-    let vec2 = vec![4, 5, 6];
+    // Hash maps are not enabled by default, we need to import them.
+    let mut user: HashMap<&str, &str> = HashMap::new();
 
-    if !vec2.is_empty() {
-        println!("vec2 isn't empty")
-    };
+    user.insert("foo", "bar");
 }
